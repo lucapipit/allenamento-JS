76df4e11c0cf072b30f932c408ee7f6ee102caf6
lucapipit <pipitonelucamaria@gmail.com>
2023-04-26 16:10:20 +0200

    completato motore, aggiungere puntate cash & css

--- blackJack_ex/script.js
+++ blackJack_ex/script.js
@@ -11,56 +11,90 @@ let scoreD = document.getElementById("scoreD");
 let cardsP = document.getElementById("cardsP");
 let scoreP = document.getElementById("scoreP");
 let winner = document.getElementById("winner");
+let container = document.getElementById("container");
 
 /* DOM buttons */
 let start = document.getElementById("start");
-
 let fishingP = document.getElementById("fishingP");
 let doneP = document.getElementById("doneP");
 
+
 /* addEvents */
 let fishP = fishingP.addEventListener("click", ()=>{
-    let rand = Math.ceil(Math.random()*13);
-    playerHand.push(rand);
-    scorePlayer += rand;
-    cardsP.innerText = "Hand: " + playerHand;
-    scoreP.innerText = "Score: " + scorePlayer;
-    if(scorePlayer > 21){winner.innerText = "Hai Sballato"; fishingP.style.display = "none"; doneP.style.display = "none";}
-    return scorePlayer;
+    fishingPlayer();
+    return;
 });
 let donP = doneP.addEventListener("click", ()=>{
     fishingP.style.display = "none";
     smartDesk();
+    return;
+});
+let strt = start.addEventListener("click", ()=>{
+    reset();
+    game();
+    container.style.display = "block";
+return;
 })
 
 
-function game(){
+/* Funzione Game */
+let game = function game(){
     extrDesk();
     extrPlayer();
-    
+    specialScoreP();
+    specialScoreD();
     /* turno player */
     cardsP.innerText = "Hand: " + playerHand;
     scoreP.innerText = "Score: " + scorePlayer;
     fishP;
     donP;
+    return;
 }
 
 /* Funzione Desk automatico */
 let smartDesk = function smartDesk(){
     
-    for (i = 0; i < 4; i++){
+    for (i = 0; i < 6; i++){
+        
         if(scoreDesk >= scorePlayer){
             cardsD.innerText = "Hand: " + deskHand;
             scoreD.innerText = "Score: " + scoreDesk;
             winner.innerText = "Hai Perso";
+            fishingP.style.display = "none"; 
+            doneP.style.display = "none";
             return;
         }
         fishingDesk();
         cardsD.innerText = "Hand: " + deskHand;
         scoreD.innerText = "Score: " + scoreDesk;
-        if(scoreDesk > 21){winner.innerText = "HAI VINTO!!! Il Desk ha sballato!"; return;}
+        if(scoreDesk > 21){winner.innerText = "HAI VINTO!!! Il Desk ha sballato!"; 
+            fishingP.style.display = "none"; 
+            doneP.style.display = "none"; 
+            cardsD.innerText = "Hand: " + deskHand;
+            scoreD.innerText = "Score: " + scoreDesk;
+            return;}
         
     }
+}
+
+/*LIBRERIA FUNZIONI*/
+
+/* Funzione Reset */
+let reset = function reset(){
+    deskHand = [];
+    scoreDesk = 0;
+    playerHand = [];
+    scorePlayer = 0;
+    /* innerText */
+    cardsP.innerText = "Hand: ";
+    scoreP.innerText = "Score: ";
+    cardsD.innerText = "Hand: ";
+    scoreD.innerText = "Score: ";
+    winner.innerText = "";
+    /* Buttons */
+    fishingP.style.display = "inline";
+    doneP.style.display = "inline";
+    return;
 }
 
 /* Funzioni di Fishing */
@@ -74,16 +108,24 @@ let fishingPlayer = function fishingPlayer(){
     let rand = Math.ceil(Math.random()*13);
     playerHand.push(rand);
     scorePlayer += rand;
+    cardsP.innerText = "Hand: " + playerHand;
+    scoreP.innerText = "Score: " + scorePlayer;
+    if(scorePlayer > 21){winner.innerText = "Hai Sballato"; fishingP.style.display = "none"; doneP.style.display = "none";}
     return scorePlayer;
 }
-
 
 /* Funzioni di estrazione */
 let extrDesk = function extrDesk(){
     for (i = 0; i < 2; i++){
         let rand = Math.ceil(Math.random()*13);
         deskHand.push(rand);
-        scoreDesk += rand;
+        
+        if(rand == 11||rand == 12||rand == 13){
+            scoreDesk += 10;
+        }else{
+            scoreDesk += rand;
+        }
+        
     }
     return scoreDesk;
 }
@@ -91,23 +133,30 @@ let extrPlayer = function extrPlayer(){
     for (i = 0; i < 2; i++){
         let rand = Math.ceil(Math.random()*13);
         playerHand.push(rand);
-        scorePlayer += rand;
+
+        if(rand == 11||rand == 12||rand == 13){
+            scorePlayer += 10;
+        }else{
+            scorePlayer += rand;
+        }
+        
     }
     return scorePlayer;
 }
 
-/* 
-console.log("Desk Score: " + extrDesk());
-console.log(deskHand);
-console.log("Player Score: " + extrPlayer());
-console.log(playerHand);
+/* assegnazione score corretto */
+let specialScoreP = function specialScoreP(){
+    if((playerHand.includes(11)||playerHand.includes(12)||playerHand.includes(13))&&playerHand.includes(1)){
+        scorePlayer = 21;
+    }
+    return;
+}
+let specialScoreD = function specialScoreD(){
+    if((deskHand.includes(11)||deskHand.includes(12)||deskHand.includes(13))&&deskHand.includes(1)){
+        scoreDesk = 21;
+    }
+    return;
+}
 
-console.log(fishingDesk());
-console.log(deskHand);
-console.log(fishingPlayer());
-console.log(playerHand);
- */
-console.log(game());
 
-/* innerText */
 
